e97576a89fba9bd628bc4904550533c9aca4bae3
zhuguoliang <zhugl@taofen8.com>
2019-08-16 10:58:49 +0800

    修改前端静态文件

---
+++ public/precache-manifest.318340cf2bad8604fe9d3465c5620d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cb60c84d4f3c2d9c1ef2a01c303198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e52746a3dcc5ebccce3",
+    "url": "/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "76b25637d8bcb8d810cc",
+    "url": "/static/css/main.907a72c7.chunk.css"
+  },
+  {
+    "revision": "3e52746a3dcc5ebccce3",
+    "url": "/static/js/2.c9896af4.chunk.js"
+  },
+  {
+    "revision": "76b25637d8bcb8d810cc",
+    "url": "/static/js/main.6baa2735.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c530ca68eb514ea6c7959c5db7aa0e8.js"
+  "/precache-manifest.318340cf2bad8604fe9d3465c5620d7b.js"
 );
 
 self.addEventListener('message', (event) => {

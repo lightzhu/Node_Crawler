67ae5e2929d29d2f193ba5378dae841e7e05f2f1
zhuguoliang <zhugl@taofen8.com>
2019-08-21 17:38:04 +0800

    无

--- mongoose/UserModel.js
+++ mongoose/UserModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const { USERDB } = require('../dbConfig.js');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const { USERDB } = require('../dbConfig.js')
 const dbUser = mongoose.createConnection(USERDB, {
   useNewUrlParser: true
-});
-dbUser.on('error', console.error.bind(console, 'connection error:'));
+})
+dbUser.on('error', console.error.bind(console, 'connection error:'))
 dbUser.once('open', function() {
   // we're connected!
-  console.log('用户数据库连接成功');
+  console.log('用户数据库连接成功')
   //实体的实例化
-});
+})
 
 //定义模式User_Schema
 //{versionKey:false}是否建立文档的版本
@@ -23,8 +23,8 @@ let User_Schema = new Schema(
   {
     versionKey: false
   }
-);
+)
 //定义模型user,注意数据库存的是users
-let User = dbUser.model('users', User_Schema);
+let User = dbUser.model('users', User_Schema)
 // 创建一个模型，这个模型对应数据库里的一个collection
-module.exports = User;
+module.exports = User

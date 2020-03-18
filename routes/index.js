2857b73348212dd1f1153e914a09198f238cc787
zhuguoliang <zhugl@taofen8.com>
2020-03-18 16:13:26 +0800

    去除用户相关路由

--- routes/index.js
+++ routes/index.js
@@ -1,94 +1,9 @@
-const path = require('path')
 const Router = require('koa-router')
 const router = new Router()
-const User = require('../mongoose/UserModel.js')
+// const User = require('../mongoose/UserModel.js')
 const MovieDetail = require('../mongoose/MovieListModel.js')
 const request = require('request')
 const NewsAppKey = 'd6b425961ff54c50c8a4fb7ceb1d63bd'
-router.get('/page', async ctx => {
-  let html = `
-    <ul>
-      <li><a href="/page/helloworld">/page/helloworld</a></li>
-      <li><a href="/page/404">/page/404</a></li>
-    </ul>
-  `
-  ctx.body = 'fdsfsdfsd'
-})
-// 注册接口
-router.post('/register', async (ctx, next) => {
-  console.log(ctx.request.body)
-  let light = new User({
-    name: ctx.request.body.userName,
-    pwd: ctx.request.body.pwd,
-    password: ctx.request.body.password,
-    hobby: ctx.request.body.hobby,
-    date: Date.now()
-  })
-  let doc = await User.findOne({ name: ctx.request.body.userName }, function (
-    err,
-    docs
-  ) {
-    if (err) {
-      console.log(err)
-    }
-  })
-  if (doc) {
-    ctx.body = {
-      code: 200,
-      msg: '用户名已存在！'
-    }
-    return false
-  }
-  let resp = await new Promise((resolve, reject) => {
-    light.save(function (err, doc) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve({
-          code: 200,
-          msg: '注册成功',
-          user: ctx.request.body.userName
-        })
-      }
-    })
-  })
-  ctx.body = resp
-})
-// 登陆接口
-router.post('/login', async (ctx, next) => {
-  // console.log(ctx.request.body);
-  let name = ctx.request.body.userName
-  let pwd = ctx.request.body.pwd
-  console.log(ctx.request.body)
-  await User.findOne({ name }, function (err, docs) {
-    if (err) {
-      console.log(err)
-    } else {
-      if (!docs) {
-        ctx.body = {
-          code: 200,
-          msg: '用户名错误',
-          statue: 1
-        }
-      } else {
-        console.log(docs)
-        if (pwd !== docs.pwd) {
-          ctx.body = {
-            code: 200,
-            msg: '密码错误',
-            statue: 2
-          }
-        } else {
-          ctx.body = {
-            code: 200,
-            msg: '登陆成功！',
-            statue: 0
-          }
-        }
-      }
-    }
-  })
-})
 
 router.get('/get_movies', async ctx => {
   console.log(ctx.query)

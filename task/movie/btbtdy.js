28d32d8cb8f0c4d9fb0ce506de3bcbea2121346d
zhuguoliang <zhugl@taofen8.com>
2020-05-28 13:36:31 +0800

    无

--- task/movie/btbtdy.js
+++ task/movie/btbtdy.js
@@ -27,106 +27,85 @@ const btbtdyfn = function (str, item) {
   })
   return list
 }
-const movieSrc = [
-  {
-    dist: 'http://www.btbtdy.la/btfl/dy1.html',
-    host: 'http://www.btbtdy.la',
-    type: 'target',
-    charset: 'UTF-8',
-    category: '1',
-    deal: btbtdyfn
-  }
-]
-// 将不同的网页请求放到一个promise数组中
-let promiseArr = []
-movieSrc.forEach((item, key) => {
-  let options = {
-    method: 'get',
-    url: item.dist
-  }
-  if (item.charset === 'gb2312') {
-    options = {
-      method: 'get',
-      url: item.dist,
-      encoding: null
-    }
-  }
-  promiseArr.push(
-    new Promise(function (resolve, reject) {
-      request(options, function (err, res) {
-        if (err) {
-          reject(err)
-        } else {
-          if (item.charset === 'gb2312') {
-            resolve(item.deal(iconv.decode(res.body, 'gb2312'), item))
-          } else {
-            resolve(item.deal(res.body.toString(), item))
-          }
-        }
-      })
-    })
-  )
-})
-
-async function updateMoviesList(data) {
-  // 根据最新的电影表获取电影信息及下载链接
-  const browser = await puppeteer.launch()
-  console.log('浏览器启动成功')
-  let movieDetailArr = []
-  let i = 0
-  while (i < data.length) {
-    let item = data[i]
-    const page = await browser.newPage()
-    await page.goto(item.dist)
-    await page.waitFor(1000);
-    let btUrl = ''
-    let content = ''
-    let postUrl = ''
-    try {
-      btUrl = await page.$eval('#video-down p:nth-of-type(2)', el => {
-        return el.innerHTML
-      })
-      content = await page.$eval('.side', el => el.innerText)
-      postUrl = await page.$eval('.side>a img', el => el.getAttribute('src'))
-    } catch (error) {
-      console.log(error)
-    }
-    if (btUrl !== '') {
-      movieDetailArr.push(new MovieDetail({
-        title: item.title,
-        id: item.id,
-        type: item.type,
-        postUrl,
-        content,
-        btUrl,
-        date: moment(new Date()).format('YYYY-MM-DD HH:mm')
-      }))
-    }
-    console.log('采集第一个页面成功')
-    sleep.sleep(1)
-    i++
-  }
-  await browser.close()
-  MovieDetail.insertMany(movieDetailArr, function (err, r) {
-    if (err) {
-      console.log(err)
-    }
-  });
+const movieSrc = {
+  dist: 'http://www.btbtdy.la/btfl/dy1.html',
+  host: 'http://www.btbtdy.la',
+  type: 'target',
+  charset: 'UTF-8',
+  category: '1',
+  deal: btbtdyfn
 }
+// 将不同的网页请求放到一个promise数组中
+function getPageDetail(item) {
+  return new Promise(function (resolve, reject) {
+    request({ method: 'get', url: item.dist }, function (err, res) {
+      if (err) {
+        reject(err)
+      } else {
+        if (item.charset === 'gb2312') {
+          resolve(item.deal(iconv.decode(res.body, 'gb2312'), item))
+        } else {
+          resolve(item.deal(res.body.toString(), item))
+        }
+      }
+    })
+  })
+}
+
+// async function updateMoviesList(data) {
+//   // 根据最新的电影表获取电影信息及下载链接
+//   const browser = await puppeteer.launch()
+//   console.log('浏览器启动成功')
+//   let movieDetailArr = []
+//   let i = 0
+//   while (i < data.length) {
+//     let item = data[i]
+//     const page = await browser.newPage()
+//     await page.goto(item.dist)
+//     await page.waitFor(1000);
+//     let btUrl = ''
+//     let content = ''
+//     let postUrl = ''
+//     try {
+//       btUrl = await page.$eval('#video-down p:nth-of-type(2)', el => {
+//         return el.innerHTML
+//       })
+//       content = await page.$eval('.side', el => el.innerText)
+//       postUrl = await page.$eval('.side>a img', el => el.getAttribute('src'))
+//     } catch (error) {
+//       console.log(error)
+//     }
+//     if (btUrl !== '') {
+//       movieDetailArr.push(new MovieDetail({
+//         title: item.title,
+//         id: item.id,
+//         type: item.type,
+//         postUrl,
+//         content,
+//         btUrl,
+//         date: moment(new Date()).format('YYYY-MM-DD HH:mm')
+//       }))
+//     }
+//     console.log('采集第一个页面成功')
+//     sleep.sleep(1)
+//     i++
+//   }
+//   await browser.close()
+//   MovieDetail.insertMany(movieDetailArr, function (err, r) {
+//     if (err) {
+//       console.log(err)
+//     }
+//   });
+// }
 
 module.exports = {
   run: function () {
     // 所有数据源拿到之后更新电影表
-    let movieList = [];
     let newMovies = []
     return new Promise(function (reslove, reject) {
-      console.log(promiseArr.length)
-      Promise.all(promiseArr)
+      getPageDetail(movieSrc)
         .then(result => {
-          for (let i = 0; i < result.length; i++) {
-            movieList = movieList.concat(result[i])
-          }
-          console.log(movieList)
+          console.log(result)
           let titles = []
           Movie.find({}, { title: 1 }, (err, docs) => {
             if (err) {
@@ -135,7 +114,7 @@ module.exports = {
               docs.forEach((item) => {
                 titles.push(item.title)
               })
-              movieList.forEach((item, index) => {
+              result.forEach((item, index) => {
                 if (!titles.includes(item.title)) {
                   newMovies.push(item)
                 }

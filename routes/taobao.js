5f275f0981380c33318c9e3f23804b42dd730906
zhuguoliang <zhugl@taofen8.com>
2020-09-15 17:13:26 +0800

    none

--- routes/taobao.js
+++ routes/taobao.js
@@ -1,35 +1,7 @@
 const Router = require('koa-router')
 const router = new Router()
-const { dingTalkSign } = require('../utils/index');
-const request = require('request')
-const moment = require('moment')
+
 const { loginToDahanghai } = require('../task/taobao.js')
-// 钉钉群发消息URL 和 secret
-let webhook = ''
-let secret = ''
-if (process.env.webhook && process.env.secret) {
-  webhook = process.env.webhook
-  secret = process.env.secret
-} else {
-  const config = require('../dbConfig')
-  webhook = config.webhook
-  secret = config.secret
-}
-let options = {
-  headers: {
-    "Content-Type": 'application/json;charset=utf-8'
-  },
-  json: {
-    "msgtype": "text",
-    "text": {
-      "content": 'T^T 测试'
-    },
-    "at": {
-      "atMobiles": [],
-      "isAtAll": false
-    }
-  }
-}
 //模拟淘宝登陆
 router.get('/taobao_login', async ctx => {
   let res = await loginToDahanghai()
--- task/taobao.js
+++ task/taobao.js
@@ -3,42 +3,6 @@ const cheerio = require('cheerio')
 const iconv = require('iconv-lite')
 const moment = require('moment')
 
-const dytt8fn = function (str, item) {
-  let $ = cheerio.load(str)
-  let list = []
-  $('.co_content8 ul table').each(function (key, value) {
-    let $tag = $(this)
-      .find('tr')
-      .eq(1)
-    let title = $tag
-      .find('td')
-      .eq(1)
-      .find('a').eq(1)
-      .text()
-    let dist =
-      item.host +
-      $tag
-        .find('td')
-        .eq(1)
-        .find('a').eq(1)
-        .attr('href')
-    let id = dist.replace(/[^0-9]/gi, '')
-    let content = $tag
-      .find('td')
-      .eq(1)
-      .find('a')
-    list.push({
-      id,
-      title,
-      type: item.type,
-      category: item.category,
-      dist,
-      date: moment(new Date()).format('YYYY-MM-DD HH:mm'),
-      content: ''
-    })
-  })
-  return list
-}
 const login = 'https://login.taobao.com/member/login.jhtml?redirectURL=https%3A%2F%2Fdahanghai.taobao.com%2F%23%2Fchannel%2FadvertisingList'
 
 const formData = {
@@ -46,14 +10,14 @@ const formData = {
   fromSite: '0',
   umidToken: '5c674d8e1851ca5f8106a38e611b28783851cad5'
 }
-if (process.env.taobaoId) {
-  formData.taobaoId = process.env.taobaoId
-  formData.taobaoPwd = process.env.taobaoPwd
-} else {
-  const DB = require('../dbConfig')
-  formData.taobaoId = DB.taobaoId
-  formData.taobaoPwd = DB.taobaoPwd
-}
+// if (process.env.taobaoId) {
+//   formData.taobaoId = process.env.taobaoId
+//   formData.taobaoPwd = process.env.taobaoPwd
+// } else {
+//   const DB = require('../dbConfig')
+//   formData.taobaoId = DB.taobaoId
+//   formData.taobaoPwd = DB.taobaoPwd
+// }
 function loginToDahanghai() {
   return new Promise(function (reslove, reject) {
     request.post({ url: 'https://login.taobao.com/newlogin/login.do', formData: formData }, (err, httpResponse, body) => {
